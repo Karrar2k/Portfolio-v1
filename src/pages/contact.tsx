84a17bbf9e4bd2e02ccd7aa36f9bd07cf2ee1a3d
Karrar2k <kalmayalis@gmail.com>
2025-01-13 09:06:35 +0000

    Fix#2

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -30,7 +30,7 @@ export default function ContactPage() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           ...formData,
-          apikey: "972dc504-80e9-4fff-8d08-b6b55056ce23",
+          apikey: process.env.NEXT_PUBLIC_WEB3FORMS_API_KEY,
         }),
       });
 
